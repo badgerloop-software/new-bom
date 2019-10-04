55f0b30f121e450efd6c21fc321f2a43039cdb9c
Luke Houge <lukehouge@gmail.com>
2019-10-04 00:20:34 -0500

    fixing env config

--- config/mongo.js
+++ config/mongo.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 
 module.exports = {
   bomURL : 'mongodb://localhost:27017/devBLBOM',
-  prodURL: 'mongodb://localhost:27017/' + config.crudDB
+  prodURL: 'mongodb://localhost:27017/' + process.env.crudDB
 }
