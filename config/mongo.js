6f9d3ab312d2dffbfc5c83bd89de4ed0fbbecbcc
Luke Houge <lukehouge@gmail.com>
2019-10-04 00:24:00 -0500

    changing .env call

--- config/mongo.js
+++ config/mongo.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
+let creds = require(".env");
 
 module.exports = {
   bomURL : 'mongodb://localhost:27017/devBLBOM',
-  prodURL: 'mongodb://localhost:27017/' + process.env.crudDB
+  prodURL: 'mongodb://localhost:27017/' + creds.crudDB
 }
