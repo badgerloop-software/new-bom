0f595e3bbbf9140cab447985ebb0f2fb0d1d654c
Luke Houge <lukehouge@gmail.com>
2019-10-04 00:32:13 -0500

    changing other urls for dev/prod

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+let creds = require("creds.json");
 
 const PORT = process.env.PORT || 7001;
 console.log(process.env.CLIENT_ID);
@@ -110,7 +111,7 @@ app.post('/teamleads/:id/delete', teamleadscontroller.teamleads_delete);
 app.post('/sponsors/upload', uploadSponsor.single('sponsorImg'), (req, res) => {
   if (req.file) {
     console.log('Uploading file...');
-    fs.rename('uploads/sponsors/' + req.file.filename, process.env.IMAGES_FOLDER + '/sponsors/' + req.file.originalname, function (err) {
+    fs.rename('uploads/sponsors/' + req.file.filename, creds.IMAGES_FOLDER + '/sponsors/' + req.file.originalname, function (err) {
       if (err) console.log('ERROR: ' + err);
     });
     // shell.mv('uploads/sponsors/' + req.file.filename', 'file2', 'dir/');
@@ -128,7 +129,7 @@ app.post('/sponsors/upload', uploadSponsor.single('sponsorImg'), (req, res) => {
 app.post('/teamleads/upload', uploadTeamlead.single('teamleadImg'), (req, res) => {
   if (req.file) {
     console.log('Uploading file...');
-    fs.rename('uploads/teamleads/' + req.file.filename, process.env.IMAGES_FOLDER + 'teamleads/' + req.file.originalname, function (err) {
+    fs.rename('uploads/teamleads/' + req.file.filename, creds.IMAGES_FOLDER + 'teamleads/' + req.file.originalname, function (err) {
       if (err) console.log('ERROR: ' + err);
     });
     // shell.mv('uploads/sponsors/' + req.file.filename', 'file2', 'dir/');
--- config/mongo.js
+++ config/mongo.js
@@ -1,6 +1,6 @@
 let creds = require("../creds.json");
 
 module.exports = {
-  bomURL : 'mongodb://localhost:27017/devBLBOM',
+  bomURL : 'mongodb://localhost:27017/' + creds.bomDB,
   prodURL: 'mongodb://localhost:27017/' + creds.crudDB
 }
