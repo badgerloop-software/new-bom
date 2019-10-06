58974a16b286d33d32f9b66a3ed3ee738afeacea
Eric <coolspot1000@gmail.com>
2019-10-06 17:24:42 -0500

    More db changes

--- config/mongo.js
+++ config/mongo.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const BOM = process.env.bomDB;
 const CRUD = process.env.crudDB;
-
+console.log(process.env);
 module.exports = {
   bomURL : 'mongodb://localhost:27017/' + BOM,
   prodURL: 'mongodb://localhost:27017/' + CRUD
