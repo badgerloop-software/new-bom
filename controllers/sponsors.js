7af36dfda863b5af4cb3b44cb7ec97432d752aa2
Luke Houge <lukehouge@gmail.com>
2019-10-12 13:27:17 -0500

    adding date functions back

--- controllers/sponsors.js
+++ controllers/sponsors.js
@@ -2,6 +2,14 @@ const sponsors = require('../models/sponsor');
 const Sponsors = require('../models/sponsor');
 const Logs = require('../models/log');
 
+let date_ob = new Date();
+let date = ("0" + date_ob.getDate()).slice(-2);
+let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+let year = date_ob.getFullYear();
+let hours = date_ob.getHours();
+let minutes = date_ob.getMinutes();
+let seconds = date_ob.getSeconds();
+
 exports.sponsors_create = function (req, res) {
     let sponsors = new Sponsors(
         {
--- controllers/teamleads.js
+++ controllers/teamleads.js
@@ -2,6 +2,14 @@ const teamleads = require('../models/teamlead');
 const Teamleads = require('../models/teamlead');
 const Logs = require('../models/log');
 
+let date_ob = new Date();
+let date = ("0" + date_ob.getDate()).slice(-2);
+let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+let year = date_ob.getFullYear();
+let hours = date_ob.getHours();
+let minutes = date_ob.getMinutes();
+let seconds = date_ob.getSeconds();
+
 exports.teamleads_create = function (req, res) {
     let teamleads = new Teamleads(
         {
