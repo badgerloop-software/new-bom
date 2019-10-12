dbfc4a4d8baf8a1ddfceaba3e240271fd445cf78
Luke Houge <lukehouge@gmail.com>
2019-10-11 22:19:34 -0500

    adding controller for logs

---
+++ controllers/crudLog.js
@@ -0,0 +1,15 @@
+const Logs = require('../models/log');
+
+exports.getCrudLogs = (req, res) => {
+    if (!req.user || !req.user.isAdmin) {
+        req.flash('errors', { msg: 'You are not authorized to view that!' });
+        return res.redirect('back');
+    }
+    Logs.find({}, (err, logsList) => {
+        if (err) throw err;
+        return res.render('crudLog', {
+            user: req.user,
+            logs: logsList,
+        });
+    });
+}
