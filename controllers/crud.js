a2168db89535bf851407da4d7006c7e10add85b5
Luke Houge <lukehouge@gmail.com>
2019-10-15 17:12:15 -0500

    fixing (hopefully) reading news articles

--- controllers/crud.js
+++ controllers/crud.js
@@ -1,22 +1,26 @@
 const Sponsors = require('../models/sponsor');
 const Teamleads = require('../models/teamlead');
+const News = require('../models/news');
 
 exports.getCrud = (req, res) => {
     if (!req.user || !req.user.isTeamLead) {
-      req.flash('errors', {msg: 'You are not authorized to view that!'});
-      return res.redirect('back');  
+        req.flash('errors', { msg: 'You are not authorized to view that!' });
+        return res.redirect('back');
     }
     Sponsors.find({}, (err, spnsrList) => {
-        if (err) throw err; 
-    Teamleads.find({}, (err, leadsList) => {
         if (err) throw err;
-	console.log(leadsList);
-        return res.render('crud', {
-            user: req.user,
-            activeCRUD: true,
-            teamleads: leadsList,
-            sponsors:  spnsrList
+        Teamleads.find({}, (err, leadsList) => {
+            if (err) throw err;
+            News.find({}, (err, newsList) => {
+                if (err) throw err;
+                return res.render('crud', {
+                    user: req.user,
+                    activeCRUD: true,
+                    teamleads: leadsList,
+                    sponsors: spnsrList,
+                    news: newsList
+                });
+            });
         });
-    });
     });
 }
