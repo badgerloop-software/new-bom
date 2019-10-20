ef36d101dc051f6688c80c9e0afce4e9f2ef74ac
Eric <coolspot1000@gmail.com>
2019-10-19 19:57:48 -0500

    Budgets now only process on ordering

--- config/mongo.js
+++ config/mongo.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const BOM = process.env.BOMDB;
 const CRUD = process.env.CRUDDB;
-console.log(process.env);
 module.exports = {
   bomURL : 'mongodb://localhost:27017/' + BOM,
   prodURL: 'mongodb://localhost:27017/' + CRUD
