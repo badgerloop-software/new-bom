e3b7510626ed1fbdbe36c7d549dcc36bcdc0a8a8
Luke Houge <lukehouge@gmail.com>
2019-11-05 20:41:54 -0600

    fixing message

--- controllers/orders.js
+++ controllers/orders.js
@@ -369,7 +369,7 @@ exports.getApproving = (req, res) => {
           if (err) throw err;
           order.save((err) => {
             if (err) throw err;
-            createSlackMessage(order, user);
+            createSlackResponse(order, user);
             req.flash('success', { msg: 'Order Approved' });
             return res.redirect('/orders/view');
           });
