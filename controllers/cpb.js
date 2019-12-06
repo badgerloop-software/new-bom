d283e1a26a7903f40af00afec0bfaeeb782d94fe
Luke Houge <lukehouge@gmail.com>
2019-12-05 22:26:27 -0600

    adding critical path files

---
+++ controllers/cpb.js
@@ -0,0 +1,15 @@
+const Logs = require('../models/log');
+
+exports.getCriticalPaths = (req, res) => {
+    if (!req.user || !req.user.isAdmin) {
+        req.flash('errors', { msg: 'You are not authorized to view that!' });
+        return res.redirect('back');
+    }
+    Logs.find({}, (err, logsList) => {
+        if (err) throw err;
+        return res.render('crudLog', {
+            logs: logsList,
+            activeCPB: true
+        });
+    });
+}
---
+++ controllers/criticalPaths.js
@@ -0,0 +1,59 @@
+const cp = require('../models/cp');
+const Cp = require('../models/cp');
+
+exports.sponsors_create = function (req, res) {
+    let cp = new Cp(
+        {
+            title: req.body.title,
+            description: req.body.description,
+            assignee: req.body.assignee,
+            due: req.body.due,
+        }
+    );
+
+    cp.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', { msg: `Critical Path created successfully!` });
+        return res.redirect('/cpb');
+    });
+    logs.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+    });
+};
+
+exports.cp_details = function (req, res) {
+    cp.findById(req.params.id, function (err, cp) {
+        if (err) throw err;
+        res.send(cp);
+    });
+};
+
+exports.cp_list = function (req, res) {
+    sponsors.find(function (err, cp) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(cp);
+        }
+    });
+};
+
+exports.cp_update = function (req, res) {
+    cp.findByIdAndUpdate(req.params.id, { $set: req.body }, function (err, cp) {
+        if (err) return next(err);
+        req.flash('success', { msg: `Critical Path updated successfully!` });
+        return res.redirect('/cpb');
+    });
+};
+
+exports.cp_delete = function (req, res) {
+    cp.findByIdAndRemove(req.params.id, function (err) {
+        if (err) return next(err);
+        req.flash('success', { msg: `Critical Path deleted successfully!` });
+        return res.redirect('/cpb');
+    });
+};
