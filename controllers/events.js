db9b0322023d74924ac437a1cb2f2a6fe7fac76b
Luke Houge <lukehouge@gmail.com>
2019-12-08 20:54:15 -0600

    adding scheduling and message  sending for cpb

--- controllers/events.js
+++ controllers/events.js
@@ -1,10 +1,32 @@
-// I started this, then I realized this needs to be on a development server before I can interact with the API.
-// TODO Later
+var request = require("request");
+const cps = require('../models/cp');
+var schedule = require('node-schedule');
 
-const url = require('url');
-
-exports.getEvent = (req, res, next) => {
-  let q = url.parse(req.url, true).query;
-  res.setHeader(200, {'content-type': 'text/plain'});
-  res.send(q.challenge);
-}
+var j = schedule.scheduleJob('*/1 * * * *', function (fireDate) { //uses node-schedule to run once every minute (cron format)
+  cps.find({}, (err, cpsList) => {
+    if (err) throw err;
+    console.log(cpsList);
+    let msg =
+      `==== CURRENT CRITICAL PATH ====
+    *Title*: ${cpsList[0].title}
+    *Description*: ${cpsList[0].description}
+    *Assignee*: ${cpsList[0].assignee}
+    *Due* ${cpsList[0].due}`;
+    var options = {
+      msg: msg,
+      method: 'POST',
+      url: 'https://slack.com/api/chat.postMessage',
+      headers:
+      {
+        Authorization: 'Bearer xoxb-782416144855-856860949009-aIfxev3o0FIAWYSyEzRp0DeM',
+        'Content-Type': 'application/json'
+      },
+      body: { channel: 'CNS3SCTCZ', text: msg },
+      json: true
+    };
+    request(options, function (error, response, body) {
+      if (error) throw new Error(error);
+      console.log(body);
+    });
+  });
+});
