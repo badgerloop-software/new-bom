97884097db6fd3d73060e4729c1256067e37b76b
LukeHouge <lukehouge@gmail.com>
2019-12-09 11:48:36 -0600

    adding variables for date support

--- controllers/events.js
+++ controllers/events.js
@@ -2,6 +2,9 @@ var request = require("request");
 const cps = require('../models/cp');
 var schedule = require('node-schedule');
 const URL = process.env.APPS_TOKEN;
+var one_day=1000*60*60*24;
+today=new Date();
+var reveal=new Date(today.getFullYear()+1, 3, 4);
 
 var j = schedule.scheduleJob('*/1 * * * *', function (fireDate) { //uses node-schedule to run once every day at 10am (cron format)
   cps.find({}, (err, cpsList) => {
