1cc8cfba9ac2e5b53810882bd41701d42f194d8d
Luke Houge <lukehouge@gmail.com>
2019-12-11 18:38:30 -0600

    fixing reveal date

--- controllers/events.js
+++ controllers/events.js
@@ -4,7 +4,7 @@ var schedule = require('node-schedule');
 const URL = process.env.APPS_TOKEN;
 var one_day=1000*60*60*24;
 today = new Date();
-var reveal=new Date(today.getFullYear()+1, 3, 4);
+var reveal=new Date(today.getFullYear()+1, 3, 16);
 
 var j = schedule.scheduleJob('0 10 * * *', function (fireDate) { //uses node-schedule to run once every day at 10am (cron format)
   cps.find({}, (err, cpsList) => {
